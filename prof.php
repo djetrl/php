1758653890783b11ec3dc18d7e8314b72c5a720a
djetrl <82327079+djetrl@users.noreply.github.com>
2022-03-28 13:22:44 +0700

    2

--- prof.php
+++ prof.php
@@ -1,26 +1,35 @@
 <?php
-	header('Content-type: text/html; charset=utf-8');
+	require_once 'user.php';
 	session_start();
 	if (!isset($_SESSION['reg'])) {
 		header('Location: login.php');
+	}
+	$user = new User();
 		
+	
+	if (!empty($_POST)){
+		$user -> save_to_session();
+		$user -> load_from_form();
 	}
 ?>
 <!DOCTYPE html>
-<html lang= "en">
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Профиль</title>
-	<link rel="stylesheet" href= "styles.css">
+	<meta charset="UTF-8">
+	<title>Регистрация</title>
+	<link rel="stylesheet" href="styles.css">
 </head>
-<body >
-<from action="" method="Post" >
-<h3>Данные</h3>
-<input type = "text"  name="name" placeholder="Имя"><br>
-<input type = "text"  name="surname" placeholder="Фамилия"><br>
-<input type = "text"  name="age" placeholder="Возраст"><br>
-<input type = "submit"  name="" ">
-
-</from>
+<body>
+	<div id="content">
+		<form action="" method="post">
+			<h3>Профиль</h3>
+			<input type="text" name="name" value=' <?php echo $user -> get_name(); ?>'  placeholder="Имя"><br>	
+			<input type="text" name="surname" value='<?php echo $user -> get_surname(); ?>' placeholder="Фамилия"><br>
+			<input type="text" name="age" value=' <?php echo $user -> get_age(); ?>' placeholder="Возраст"><br>
+			<input type="submit" value="Сохранить" name="" >
+		</form>
+	</div>
 </body>
 </html>
+
+
